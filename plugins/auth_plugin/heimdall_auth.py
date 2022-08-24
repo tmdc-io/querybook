7553a2189cbaf39f04f9365976f45f57f018f737
Animesh Kumar <32766+anismiles@users.noreply.github.com>
2022-08-25 02:36:23 +0530

    Feat: support apikey or Authentication header to access backend apis

--- plugins/auth_plugin/heimdall_auth.py
+++ plugins/auth_plugin/heimdall_auth.py
@@ -10,9 +10,10 @@ from logic.user import get_user_by_name, create_user, update_user_properties
 from app.auth.utils import (
     AuthUser,
     abort_unauthorized,
-    AuthenticationError
+    AuthenticationError,
+    get_dataos_user_profile,
+    get_or_create_dataos_user_apikey
 )
-
 from app.auth.oauth_auth import OAuthLoginManager
 from env import QuerybookSettings, get_env_config, get_env_config_strip_slash
 from lib.logger import get_logger
@@ -47,11 +48,11 @@ class HeimdallLoginManager(OAuthLoginManager):
 
         try:
             access_token = self._fetch_access_token(code)
-            username, email, fullname = self._get_user_profile(access_token)
-            apikey = self._get_heimdall_apikey(username, access_token)
+            username, email, fullname, tags = get_dataos_user_profile(access_token)
+            user_apikey = get_or_create_dataos_user_apikey(username, access_token)
             with DBSession() as session:
                 flask_login.login_user(
-                    AuthUser(self.login_user(username, email, apikey, session=session, fullname=fullname))
+                    AuthUser(self.login_user(username, email, user_apikey, tags, session=session, fullname=fullname))
                 )
         except AuthenticationError:
             abort_unauthorized()
@@ -64,39 +65,38 @@ class HeimdallLoginManager(OAuthLoginManager):
         return redirect(next_url)
 
     @with_session
-    def login_user(self, username, email, apikey, session=None, fullname=None):
+    def login_user(self, username, email, user_apikey, tags, session=None, fullname=None):
         user = get_user_by_name(username, session=session)
         if not user:
             user = create_user(
-                username=username, fullname=fullname if fullname is not None else username, email=email, session=session
+                username=username,
+                fullname=fullname if fullname is not None else username,
+                email=email,
+                session=session,
+                properties={'heimdall': user_apikey, 'tags': tags},
             )
-        if apikey:
-            update_user_properties(user.id, heimdall=apikey, fullname=fullname if fullname is not None else username)
-        if fullname:
-            update_user_properties(user.id, fullname=fullname)
+        else:
+            # TODO: Update user's fullname if that has changed
+            update_user_properties(
+                user.id,
+                heimdall=user_apikey,
+                tags=tags
+           )
 
         return user
-
-    def get_oidc_urls(self):
-        dataos_base_url = QuerybookSettings.DATAOS_BASE_URL
-        authorization_url = f"{dataos_base_url}/oidc/auth"
-        token_url = f"{dataos_base_url}/oidc/token"
-        profile_url = f"{dataos_base_url}/oidc/userinfo"
-
-        return authorization_url, token_url, profile_url
-
-    def get_oidc_secrets(self):
-        client_id = QuerybookSettings.DATAOS_OIDC_CLIENT_ID
-        client_secret = QuerybookSettings.DATAOS_OIDC_CLIENT_SECRET
-
-        return client_id, client_secret
 
     @property
     @in_mem_memoized()
     def oauth_config(self):
-        authorization_url, token_url, profile_url = self.get_oidc_urls()
-        client_id, client_secret = self.get_oidc_secrets()
+        client_id = QuerybookSettings.DATAOS_OIDC_CLIENT_ID
+        client_secret = QuerybookSettings.DATAOS_OIDC_CLIENT_SECRET
+        dataos_base_url = QuerybookSettings.DATAOS_BASE_URL
+
         callback_url = "{}{}".format(QuerybookSettings.PUBLIC_URL, OIDC_CALLBACK_PATH)
+        authorization_url = f"{dataos_base_url}/oidc/auth"
+        token_url = f"{dataos_base_url}/oidc/token"
+        profile_url = f"{dataos_base_url}/oidc/userinfo"
+        scope = ["openid", "profile", "email", "groups", "federated:id"]
 
         return {
             "callback_url": callback_url,
@@ -105,87 +105,8 @@ class HeimdallLoginManager(OAuthLoginManager):
             "authorization_url": authorization_url,
             "token_url": token_url,
             "profile_url": profile_url,
-            "scope": ["openid", "profile", "email", "groups", "federated:id"],
+            "scope": scope,
         }
-
-    def _get_user_profile(self, access_token):
-        dataos_base_url = QuerybookSettings.DATAOS_BASE_URL
-        heimdall_base_url = f"{dataos_base_url}/heimdall"
-
-        # Authorize
-        heimdall_auth_url = f"{heimdall_base_url}/api/v1/authorize"
-        LOG.debug(f"[Heimdall] auth_url: {heimdall_auth_url}")
-
-        resp = requests.post(heimdall_auth_url, json={"token": access_token})
-        LOG.debug(f"[Heimdall] resp: {resp.status_code}")
-        if resp and resp.status_code == 200:
-            reply = resp.json()
-            if reply["allow"] and reply["result"] is not None:
-                user_id = reply["result"]["id"]
-
-                # Profile
-                heimdall_profile_url = f"{heimdall_base_url}/api/v1/users/{user_id}"
-                LOG.debug(f"[Heimdall] profile_url: {heimdall_profile_url}")
-
-                headers = {"Authorization": "Bearer {}".format(access_token)}
-                resp = requests.get(heimdall_profile_url, headers=headers)
-                LOG.debug(f"[Heimdall] resp: {resp.status_code}")
-                if resp.status_code == 200:
-                    return self._parse_user_profile(resp)
-                else:
-                    raise AuthenticationError(
-                        "Failed to fetch user profile, status ({0}), body ({1})".format(
-                            resp.status if resp else "None",
-                            resp.json() if resp else "None",
-                        )
-                    )
-        else:
-            raise AuthenticationError(
-                "Failed to authorize with Heimdall, status ({0}), body ({1})".format(
-                    resp.status if resp else "None", resp.json() if resp else "None"
-                )
-            )
-
-    def _parse_user_profile(self, resp):
-        user = resp.json()
-        LOG.info(f"[Heimdall] resolved user: {user}")
-        return user["id"], user["email"], user["name"]
-
-    def _get_heimdall_apikey(self, user_id, access_token):
-        dataos_base_url = QuerybookSettings.DATAOS_BASE_URL
-        heimdall_base_url = f"{dataos_base_url}/heimdall"
-        heimdall_apikey_url = f"{heimdall_base_url}/api/v1/users/{user_id}/tokens"
-
-        user_hash = hashlib.md5(user_id.encode()).hexdigest()
-        querybook_token_name = "querybook_{}".format(user_hash)
-
-        headers = {"Authorization": "Bearer {}".format(access_token)}
-        json = {
-            "use_existing": True,
-            "type": "apikey",
-            "name": querybook_token_name,
-            "duration": "8760h"
-        }
-
-        LOG.debug(f"[Heimdall] apikey_url: {heimdall_apikey_url} json: {json}")
-
-        resp = requests.post(
-            heimdall_apikey_url,
-            headers=headers,
-            json=json,
-        )
-
-        LOG.debug(f"[Heimdall] resp: {resp.status_code} {resp.json()}")
-
-        if resp.status_code == 200 or resp.status_code == 201:
-            token = resp.json()
-            return token["data"]["apikey"]
-        else:
-            raise AuthenticationError(
-                "Failed to get_or_create apikey from Heimdall, status ({0}), body ({1})".format(
-                    resp.status if resp else "None", resp.json() if resp else "None"
-                )
-            )
 
 
 login_manager = HeimdallLoginManager()
--- querybook/server/app/auth/utils.py
+++ querybook/server/app/auth/utils.py
@@ -1,3 +1,5 @@
+import requests
+import hashlib
 import flask
 from flask_login import UserMixin, LoginManager
 from flask import abort, session as flask_session
@@ -11,7 +13,16 @@ from models.user import User
 from app.db import DBSession, get_session
 from logic.admin import get_api_access_token
 from logic.environment import get_all_accessible_environment_ids_by_uid
-from logic.user import get_user_by_id
+from logic.user import (
+    get_user_by_name,
+    get_user_by_id,
+    create_user,
+    # update_user_properties,
+)
+from env import QuerybookSettings
+from lib.logger import get_logger
+
+LOG = get_logger(__file__)
 
 
 class AuthenticationError(Exception):
@@ -77,6 +88,29 @@ def load_user_with_api_access_token(request):
                     flask.abort(401, description="Token is disabled.")
             else:
                 flask.abort(401, description="Token is invalid.")
+
+    bearer_token = request.headers.get("Authorization")
+    if bearer_token:
+        token_string = bearer_token.lstrip("Bearer").strip()
+    if not bearer_token:
+        token_string = request.headers.get("apikey")
+
+    if token_string:
+        user_id = authorize_with_heimdall(token_string)
+        with DBSession() as session:
+            user = get_user_by_name(user_id, session=session)
+            if not user:
+                username, email, fullname, tags = get_dataos_user_profile(token_string)
+                user_apikey = get_or_create_dataos_user_apikey(username, token_string)
+                user = create_user(
+                    username=username,
+                    fullname=fullname if fullname is not None else username,
+                    email=email,
+                    session=session,
+                    properties={"heimdall": user_apikey, "tags": tags},
+                )
+            return AuthUser(user)
+
     return None
 
 
@@ -90,3 +124,107 @@ def abort_unauthorized():
 
 def abort_forbidden():
     abort(ACCESS_RESTRICTED_STATUS_CODE)
+
+
+def authorize_with_heimdall(access_token):
+    dataos_base_url = QuerybookSettings.DATAOS_BASE_URL
+    heimdall_base_url = f"{dataos_base_url}/heimdall"
+
+    # Authorize
+    heimdall_auth_url = f"{heimdall_base_url}/api/v1/authorize"
+    LOG.debug(f"[Heimdall] auth_url: {heimdall_auth_url}")
+
+    resp = requests.post(heimdall_auth_url, json={"token": access_token})
+    LOG.debug(f"[Heimdall] resp: {resp.status_code}")
+
+    if resp and resp.status_code == 200:
+        reply = resp.json()
+        LOG.debug(f"[Heimdall] reply: {reply}")
+        if reply["allow"] and reply["result"] is not None:
+            user_id = reply["result"]["id"]
+            return user_id
+
+    raise AuthenticationError(
+        "Failed to authorize with Heimdall, status ({0}), body ({1})".format(
+            resp.status if resp else "None", resp.json() if resp else "None"
+        )
+    )
+
+
+def get_dataos_user_profile(access_token):
+    dataos_base_url = QuerybookSettings.DATAOS_BASE_URL
+    heimdall_base_url = f"{dataos_base_url}/heimdall"
+
+    # Authorize
+    heimdall_auth_url = f"{heimdall_base_url}/api/v1/authorize"
+    LOG.debug(f"[Heimdall] auth_url: {heimdall_auth_url}")
+
+    resp = requests.post(heimdall_auth_url, json={"token": access_token})
+    LOG.debug(f"[Heimdall] resp: {resp.status_code}")
+    if resp and resp.status_code == 200:
+        reply = resp.json()
+        LOG.debug(f"[Heimdall] reply: {reply}")
+        if reply["allow"] and reply["result"] is not None:
+            user_id = reply["result"]["id"]
+
+            # Profile
+            heimdall_profile_url = f"{heimdall_base_url}/api/v1/users/{user_id}"
+            LOG.debug(f"[Heimdall] profile_url: {heimdall_profile_url}")
+
+            headers = {"Authorization": "Bearer {}".format(access_token)}
+            resp = requests.get(heimdall_profile_url, headers=headers)
+            LOG.debug(f"[Heimdall] resp: {resp.status_code}")
+            if resp.status_code == 200:
+                user = resp.json()
+                LOG.info(f"[Heimdall] resolved user: {user}")
+                return user["id"], user["email"], user["name"], user["tags"]
+            else:
+                raise AuthenticationError(
+                    "Failed to fetch user profile, status ({0}), body ({1})".format(
+                        resp.status if resp else "None",
+                        resp.json() if resp else "None",
+                    )
+                )
+    else:
+        raise AuthenticationError(
+            "Failed to authorize with Heimdall, status ({0}), body ({1})".format(
+                resp.status if resp else "None", resp.json() if resp else "None"
+            )
+        )
+
+
+def get_or_create_dataos_user_apikey(user_id, access_token):
+    dataos_base_url = QuerybookSettings.DATAOS_BASE_URL
+    heimdall_base_url = f"{dataos_base_url}/heimdall"
+    heimdall_apikey_url = f"{heimdall_base_url}/api/v1/users/{user_id}/tokens"
+
+    user_hash = hashlib.md5(user_id.encode()).hexdigest()
+    querybook_token_name = "querybook_{}".format(user_hash)
+
+    headers = {"Authorization": "Bearer {}".format(access_token)}
+    json = {
+        "use_existing": True,
+        "type": "apikey",
+        "name": querybook_token_name,
+        "duration": "8760h",
+    }
+
+    LOG.debug(f"[Heimdall] apikey_url: {heimdall_apikey_url} json: {json}")
+
+    resp = requests.post(
+        heimdall_apikey_url,
+        headers=headers,
+        json=json,
+    )
+
+    LOG.debug(f"[Heimdall] resp: {resp.status_code} {resp.json()}")
+
+    if resp.status_code == 200 or resp.status_code == 201:
+        token = resp.json()
+        return token["data"]["apikey"]
+    else:
+        raise AuthenticationError(
+            "Failed to get_or_create apikey from Heimdall, status ({0}), body ({1})".format(
+                resp.status if resp else "None", resp.json() if resp else "None"
+            )
+        )
